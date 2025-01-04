92f0d36c160c3d061da8a1c7a3731eca6a3f22d0
lightrao <lightrao@hotmail.com>
2025-01-04 15:00:47 +0800

    use api key

--- lib/network/network.dart
+++ lib/network/network.dart
@@ -1,13 +1,13 @@
 import 'package:http/http.dart' as http;
 import 'package:pd24_book_tracker_app/models/book.dart';
 import 'dart:convert';
+import 'package:pd24_book_tracker_app/env/api_key.dart';
 
 class Network {
-  // https://www.googleapis.com/books/v1/volumes?q=flutter
   static const String _baseUrl = 'https://www.googleapis.com/books/v1/volumes';
 
   Future<List<Book>> searchBooks(String query) async {
-    var url = Uri.parse('$_baseUrl?q=$query');
+    var url = Uri.parse('$_baseUrl?q=$query&key=${ApiKey.apiKey}');
     final response = await http.get(url);
     if (response.statusCode == 200) {
       var data = jsonDecode(response.body);
